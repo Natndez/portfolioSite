0869af6af8a42619097e9902050f33b5dfd1f86a
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-01-17 21:20:20 -0500

    Clicking on Web Projects now brings up an overlay (setting the foundation for modals to appear)

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Main functions used in site
+
+// Basic function to show overlay
+function showOverlay() {
+    // Get overlay element
+    var overlay = document.getElementById('overlay');
+
+    // Set overlay display to block (Shows the overlay)
+    overlay.style.display = 'block';
+}
+
+function hideOverlay() {
+    // Get overlay element
+    var overlay = document.getElementById('overlay');
+
+    // Set overlay display to block (Shows the overlay)
+    overlay.style.display = 'none';
+}
