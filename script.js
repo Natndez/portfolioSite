f408d6db0c0a9e2219f74e5ff12c7aecef7fee1a
Nathan Fernandez <fernandeznathan891@gmail.com>
2024-01-18 17:52:07 -0500

    Can now use escape key as well to close modal

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Main functions used in site
 
+
+
 // Basic function to show overlay
 function showModal(modalId) {
     // Get modal element
@@ -28,3 +30,12 @@ function hideModal() {
     // Set overlay display to none (Hides the overlay)
     overlay.style.display = 'none';
 }
+// Code to use escape key to close modal
+document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape') { // If the key pressed is the escape key
+            hideModal();
+        }
+    });
+});
+
